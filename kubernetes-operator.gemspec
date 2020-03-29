e73ff52e31f221728f9d6e5310582e43e579fd4b
TobiasKuntzsch <tobias@familykuntzsch.de>
2020-03-29 21:26:30 +0200

    switch to kubeclient

--- kubernetes-operator.gemspec
+++ kubernetes-operator.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
     s.name	= 'kubernetes-operator'
-    s.version	= '0.0.3'
+    s.version	= '0.0.4'
     s.platform    = Gem::Platform::RUBY
     s.summary	= "lib"
     s.description	= "Libary to create an kubernetes operator with ruby"
@@ -10,7 +10,7 @@ Gem::Specification.new do |s|
     s.files	=  Dir['README.md', '{bin,lib,config,vendor}/**/*'] # 'VERSION', 'Gemfile', 'Rakefile', 
     s.require_path = 'lib'
 
-    s.add_dependency('k8s-client')
+    s.add_dependency('kubeclient')
     s.add_dependency('log4r')
     s.add_dependency('log_formatter')
 
--- lib/kubernetes-operator.rb
+++ lib/kubernetes-operator.rb
@@ -1,7 +1,7 @@
 #!/usr/local/bin/ruby
 
 ## kubernetes
-require 'k8s-client'
+require 'kubeclient'
 
 ## storage
 require 'yaml'
@@ -24,6 +24,7 @@ class KubernetesOperator
         # default config
         @options = options
         @options[:sleepTimer] ||= 10
+        @options[:namespace] ||= nil
 
         # create persistence
         @options[:persistence_location] ||= "/tmp/persistence"
@@ -41,13 +42,30 @@ class KubernetesOperator
         # kubeconfig
         # (for local development it's nice to use .kube/config)
         if File.exist?("#{Dir.home}/.kube/config")
-            @k8sclient = K8s::Client.config(
-                K8s::Config.load_file(
-                    File.expand_path '~/.kube/config'
-                )
+            @logger.info("use local kube config")
+            config = Kubeclient::Config.read(ENV['KUBECONFIG'] || "#{ENV['HOME']}/.kube/config")
+            context = config.context
+            @k8sclient = Kubeclient::Client.new(
+                context.api_endpoint+"/apis/"+@crdGroup,
+                @crdVersion,
+                ssl_options: context.ssl_options,
+                auth_options: context.auth_options
             )
         else
-            @k8sclient = K8s::Client.in_cluster_config
+            @logger.info("use incluster config")
+            auth_options = {
+                bearer_token_file: '/var/run/secrets/kubernetes.io/serviceaccount/token'
+            }
+            ssl_options = {}
+            if File.exist?("/var/run/secrets/kubernetes.io/serviceaccount/ca.crt")
+                ssl_options[:ca_file] = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+            end
+            @k8sclient = Kubeclient::Client.new(
+                'https://kubernetes.default.svc/apis/'+@crdGroup,
+                @crdVersion,
+                auth_options: auth_options,
+                ssl_options:  ssl_options
+            )
         end
     end
 
@@ -86,64 +104,130 @@ class KubernetesOperator
         while true
 
             begin
-
-                # Search for ressources
-                _ressources = @k8sclient.api(@crdGroup+"/"+@crdVersion).resource(@crdPlural).list()
-
-                _ressources.each do |_i|
-                    _uid = _i["metadata"]["uid"]
-                    _v = _i["metadata"]["resourceVersion"]
-
-                    if @options[:namespace] == nil || @options[:namespace].contains(_i["metadata"]["namespace"])
-                        _from_cache = @store.transaction{@store[_uid]}
-
-                        unless _from_cache
-                            # Add finalizer and refresh version number
-                            _i[:metadata][:finalizers] = ["#{@crdPlural}.#{@crdVersion}.#{@crdGroup}"]
-                            _i2 = @k8sclient.api(@crdGroup+"/"+@crdVersion).resource(@crdPlural).update_resource(_i)
-                            _v = _i2["metadata"]["resourceVersion"]
-
-                            # Cache last version of ressources
-                            @store.transaction do
-                                @store[_uid] = _v
-                                @store.commit
+                if @options[:namespace]
+                    watcher = @k8sclient.watch_entities(@crdPlural,@options[:namespace])
+                else
+                    watcher = @k8sclient.watch_entities(@crdPlural)
+                end
+                watcher.each do |notice|
+                    begin
+                        isCached = @store.transaction{@store[notice[:object][:metadata][:uid]]}
+                        case notice[:type]
+                        # new cr was added
+                        when "ADDED"
+                            # check if version is already processed
+                            unless isCached
+                                # add finalizer
+                                @logger.info("add finalizer to #{notice[:object][:metadata][:name]} (#{notice[:object][:metadata][:uid]})")
+                                patched = @k8sclient.patch_entity(@crdPlural,notice[:object][:metadata][:name], {metadata: {finalizers: ["#{@crdPlural}.#{@crdVersion}.#{@crdGroup}"]}},'merge-patch',@options[:namespace])
+                                # trigger action
+                                @logger.info("trigger add action for #{notice[:object][:metadata][:name]} (#{notice[:object][:metadata][:uid]})")
+                                @addMethod.call(notice[:object],@k8sclient)
+                                # save version
+                                @store.transaction do
+                                    @store[patched[:metadata][:uid]] = patched[:metadata][:resourceVersion]
+                                    @store.commit
+                                end
+                            else
+                                @logger.info("skip add action for #{notice[:object][:metadata][:name]} (#{notice[:object][:metadata][:uid]}), found version in cache")
                             end
-
-                            # call the action method
-                            _i["metadata"]["crd_status"] = "add"
-                            @logger.info("add custom resource #{_i["metadata"]["name"]}@#{_i["metadata"]["namespace"]}") if _i["metadata"]["namespace"]
-                            @logger.info("add custom resource #{_i["metadata"]["name"]}@cluster") unless _i["metadata"]["namespace"]
-                            @addMethod.call(_i,@k8sclient)
-                        else
-                            # only trigger action on change or delete event
-                            unless _from_cache == _v
-                                if _i["metadata"]["deletionTimestamp"]
-                                    # remove finalizers
-                                    _i[:metadata][:finalizers] = []
-                                    @k8sclient.api(@crdGroup+"/"+@crdVersion).resource(@crdPlural).update_resource(_i)
-
-                                    # call the action method
-                                    _i["metadata"]["crd_status"] = "delete"
-                                    @logger.info("delete custom resource #{_i["metadata"]["name"]}@#{_i["metadata"]["namespace"]}") if _i["metadata"]["namespace"]
-                                    @logger.info("delete custom resource #{_i["metadata"]["name"]}@cluster") unless _i["metadata"]["namespace"]
-                                    @deleteMethod.call(_i,@k8sclient)
-                                else
-                                    # store new version in cache
+                        # cr was change or deleted (if finalizer is set, it an modified call, not an delete)
+                        when "MODIFIED"
+                            # check if version is already processed
+                            if isCached != notice[:object][:metadata][:resourceVersion]
+                                # check if it's an delete event
+                                unless notice[:object][:metadata][:deletionTimestamp]
+                                    # trigger action
+                                    @logger.info("trigger update action for #{notice[:object][:metadata][:name]} (#{notice[:object][:metadata][:uid]})")
+                                    @updateMethod.call(notice[:object],@k8sclient)
+                                    # save version
                                     @store.transaction do
-                                        @store[_uid] = _v
+                                        @store[notice[:object][:metadata][:uid]] = notice[:object][:metadata][:resourceVersion]
                                         @store.commit
                                     end
-
-                                    # call the action method
-                                    _i["metadata"]["crd_status"] = "update"
-                                    @logger.info("update custom resource #{_i["metadata"]["name"]}@#{_i["metadata"]["namespace"]}") if _i["metadata"]["namespace"]
-                                    @logger.info("update custom resource #{_i["metadata"]["name"]}@cluster") unless _i["metadata"]["namespace"]
-                                    @updateMethod.call(_i,@k8sclient)
+                                else
+                                    # trigger action
+                                    @logger.info("trigger delete action for #{notice[:object][:metadata][:name]} (#{notice[:object][:metadata][:uid]})")
+                                    @updateMethod.call(notice[:object],@k8sclient)
+                                    # remove finalizer
+                                    @logger.info("remove finalizer to #{notice[:object][:metadata][:name]} (#{notice[:object][:metadata][:uid]})")
+                                    patched = @k8sclient.patch_entity(@crdPlural,notice[:object][:metadata][:name], {metadata: {finalizers: nil}},'merge-patch',@options[:namespace])
                                 end
+                            else
+                                @logger.info("skip update action for #{notice[:object][:metadata][:name]} (#{notice[:object][:metadata][:uid]}), found version in cache")
                             end
+                        when "DELETED"
+                            @logger.info("#{notice[:object][:metadata][:name]} (#{notice[:object][:metadata][:uid]}) is done")
+                        else
+                            @logger.info("strange things are going on here, I found the type "+notice[:type])
                         end
+                    rescue => exception
+                        @logger.error(exception.inspect)
                     end
                 end
+                watcher.finish
+
+                ## Search for ressources
+                #_ressources = @k8sclient.get(@crdGroup+"/"+@crdVersion).resource(@crdPlural).list()
+
+                #_ressources.each do |_i|
+                #    _uid = _i["metadata"]["uid"]
+                #    _v = _i["metadata"]["resourceVersion"]
+
+                #    if @options[:namespace] == nil || @options[:namespace].contains(_i["metadata"]["namespace"])
+                #        _from_cache = @store.transaction{@store[_uid]}
+
+                #        unless _from_cache
+                #            # Add finalizer and refresh version number
+                #            _i[:metadata][:finalizers] = ["#{@crdPlural}.#{@crdVersion}.#{@crdGroup}"]
+                #            _i = @k8sclient.api(@crdGroup+"/"+@crdVersion).resource(@crdPlural).update_resource(_i)
+
+                #            # call the action method
+                #            _i["metadata"]["crd_status"] = "add"
+                #            @logger.info("add custom resource #{_i["metadata"]["name"]}@#{_i["metadata"]["namespace"]}") if _i["metadata"]["namespace"]
+                #            @logger.info("add custom resource #{_i["metadata"]["name"]}@cluster") unless _i["metadata"]["namespace"]
+                #            @addMethod.call(_i,@k8sclient)
+
+                #            # update status
+                #            _i[:status] = {message: "Test"}
+                #            _i = @k8sclient.api(@crdGroup+"/"+@crdVersion).resource(@crdPlural).update_resource(_i)
+
+                #            # Cache last version of ressources
+                #            @store.transaction do
+                #                @store[_uid] = _i["metadata"]["resourceVersion"]
+                #                @store.commit
+                #            end
+
+                #        else
+                #            # only trigger action on change or delete event
+                #            unless _from_cache == _v
+                #                if _i["metadata"]["deletionTimestamp"]
+                #                    # remove finalizers
+                #                    _i[:metadata][:finalizers] = []
+                #                    @k8sclient.api(@crdGroup+"/"+@crdVersion).resource(@crdPlural).update_resource(_i)
+
+                #                    # call the action method
+                #                    _i["metadata"]["crd_status"] = "delete"
+                #                    @logger.info("delete custom resource #{_i["metadata"]["name"]}@#{_i["metadata"]["namespace"]}") if _i["metadata"]["namespace"]
+                #                    @logger.info("delete custom resource #{_i["metadata"]["name"]}@cluster") unless _i["metadata"]["namespace"]
+                #                    @deleteMethod.call(_i,@k8sclient)
+                #                else
+                #                    # store new version in cache
+                #                    @store.transaction do
+                #                        @store[_uid] = _v
+                #                        @store.commit
+                #                    end
+
+                #                    # call the action method
+                #                    _i["metadata"]["crd_status"] = "update"
+                #                    @logger.info("update custom resource #{_i["metadata"]["name"]}@#{_i["metadata"]["namespace"]}") if _i["metadata"]["namespace"]
+                #                    @logger.info("update custom resource #{_i["metadata"]["name"]}@cluster") unless _i["metadata"]["namespace"]
+                #                    @updateMethod.call(_i,@k8sclient)
+                #                end
+                #            end
+                #        end
+                #    end
+                #end
 
             rescue => exception
                 @logger.error(exception.inspect)
